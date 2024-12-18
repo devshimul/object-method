c50e636ca70170946881adc37058b307049e1d02
devshimul <shimul.bisd@gmail.com>
2024-12-18 22:54:13 +0600

    Object method

---
+++ script.js
@@ -0,0 +1,10 @@
+const jonas = {
+  name: "Jonas Smetmann",
+  age: 2024 - 1987,
+  job: "Teacher",
+  office: "BiTech Soft",
+};
+let userInput = prompt(
+  "What do you want to kno about jonas? between Name, Age, Job and Office"
+);
+userInput = userInput.toLowerCase();
